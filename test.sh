4913b67b179d3e3f4f7cfbef0ce071781b27fce5
Eric Kang <erickangatms@outlook.com>
2018-02-08 13:57:24 -0800

    .

--- test.sh
+++ test.sh
@@ -11,12 +11,27 @@ quit
 exit
 
 
+docker exec -ti mssql-scripter sh
+mssql-scripter -Smssql -dWideWorldImporters -Usa -PSqlDevOps2017
+exit
+
+docker exec -ti mssql-all sh
+export MSSQL_CLI_USER=sa
+export MSSQL_CLI_PASSWORD=SqlDevOps2017
+export MSSQL_CLI_SERVER=mssql
+mssql-cli
+use WideWorldImporters;
+select * from INFORMATION_SCHEMA.Tables where table_name=N'People';
+quit
+
+mssql-scripter -Smssql -dWideWorldImporters -Usa -PSqlDevOps2017
+exit
+
+
+
 docker exec -ti mssql-tools sh
 /opt/mssql-tools/bin/sqlcmd -Smssql -Usa -PSqlDevOps2017 -Q "select name from sys.databases"
 exit
 
-docker exec -ti mssql-scripter sh
-mssql-scripter -Smssql -dWideWorldImporters -Usa -PSqlDevOps2017
-exit
 
 docker-compose down
