6e437e52f8373ffc2ac02d6f68ca7e0c374ac229
Eric Kang <erickangatms@outlook.com>
2018-02-08 17:57:55 -0800

    .

--- build_all.sh
+++ build_all.sh
@@ -3,6 +3,7 @@ docker rmi -f microsoft/sqltools:cli
 docker rmi -f microsoft/sqltools:all
 docker rmi -f $(docker images -f "dangling=true" -q)
 
+docker build ./tools-python-base --rm -t sqltools-python-base:latest
 docker build ./mssql-cli --rm -t microsoft/sqltools:cli
 docker build ./mssql-scripter --rm -t microsoft/sqltools:scripter
 docker build ./mssql-all-tools --rm -t microsoft/sqltools:all
