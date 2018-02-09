3d22c67043a2a3ea81b9f6f8b8fe6be81836eb01
Eric Kang <erickangatms@outlook.com>
2018-02-09 09:24:19 -0800

    .

--- push_all.sh
+++ push_all.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+docker tag microsoft/sqltools:all ericskang/sqltools:all
+docker tag microsoft/sqltools:cli ericskang/sqltools:cli
+docker tag microsoft/sqltools:scripter ericskang/sqltools:scripter
+docker tag sqltools-python-base:latest ericskang/sqltools:python-base
+docker images
+
+docker push ericskang/sqltools:all
+docker push ericskang/sqltools:cli
+docker push ericskang/sqltools:scripter
+docker push ericskang/sqltools:python-base
